3d1cce189fc1e13f0f13e945e1ddec45a03bf07f
skyrunner360 <mathurrishi31@gmail.com>
2022-01-12 08:07:58 +0530

    Added CORS

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const connectToMongo = require('./db');
 const express = require('express');
+var cors = require('cors');
 
 
 connectToMongo();
@@ -8,11 +9,12 @@ const app = express()
 const port = 5000
 
 app.use(express.json())
+app.use(cors())
 
 // Available Routes
 app.use('/api/auth',require('./routes/auth'))
 app.use('/api/notes',require('./routes/notes'))
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+  console.log(`Noter Backend listening at http://localhost:${port}`)
 })
